ede77b654de605377619b2c4bf847f63dc35f43b
fillipesansilva <fillipesantos00@gmail.com>
2020-01-31 00:17:57 -0300

    added datasets

--- Codigos/Graph Metrics/main.py
+++ Codigos/Graph Metrics/main.py
@@ -2,64 +2,54 @@
 #Calculo das metricas de centralidades: Closeness and Betweenness Temporais.
 from temporal_graph import *
 
-dadosIn = open("graph.txt", "r")
-vertex = open("vertices.txt",'r')
+dadosIn = open("graph.txt", "r") #Graph path > Datasets/COLOGNEGraph24, FRANCE/7-9h, FRANCE/17-19h, MADRID/A6 or MADRID/M40.
+vertex = open("vertices.txt",'r') #Vertex path
 
-# dadosOut1 = open("btwTemp.txt",'w')
-# dadosOut2 = open("clsTemporal.txt",'w')
-# dadosOut3 = open("closAgregado.txt",'w')
+dadosOut1 = open("btwTemp.txt",'w') 
+dadosOut2 = open("clsTemporal.txt",'w')
+dadosOut3 = open("closAgregado.txt",'w')
 
 if __name__ == "__main__":
       
-    # et = 3 # time end
-	
-    # G = temporal_graph(et)
+    et = 3 # time end to every graph.
     
-    # ls = []
-    # for i in vertex:
-
-    #     ls.append(i.rstrip())
+    G = temporal_graph(et)
+    
+    ls = []
+    for i in vertex:
+        ls.append(i.rstrip())
    
-    # G.add_vertices(ls)
-	
-    # for v in dadosIn:
-    #     v = v.rstrip().split(" ")
-    #     time = (v[2],v[3])
-    #     vertices = (v[0],v[1])
-    #     junt = [(vertices,time)]
-    #     G.add_temporal_edges(junt)
-	
+    G.add_vertices(ls)
+    
+    for v in dadosIn:
+        v = v.rstrip().split(" ")
+        time = (v[2],v[3])
+        vertices = (v[0],v[1])
+        junt = [(vertices,time)]
+        G.add_temporal_edges(junt)
 
 
-    gg = nx.read_edgelist("graph.txt",create_using=nx.DiGraph(), nodetype = str)
-    cc=nx.average_clustering(gg) 
-    print(cc)
-
-
-    # print("[=>] Calculando betweenness Temporal")
-    # temp_betweenness = compute_temporal_betweenness(G,0,et)
-    # for a,b in temp_betweenness.items():
-    #     print(a,b)
-           #dadosOut1.write(str(a)+ " " + str(b)+"\n")
-		
-    # print("[=>] Calculando closeness Temporal")
-    # temp_closeness = compute_temporal_closeness(G,0,et)
-    # # for v in G.vertices:
-    # #     dadosOut2.write(str(v) + " " + str(temp_closeness[v]) + "\n")	
-    # print(temp_closeness)
-
-    # print("[=>] Calculando closeness Agregado")
-    # agg_closeness = compute_static_graph_statistics(G,0,et)
-    # for v in G.vertices:
-    #     if v == "d":
-    #         print(v + " " + str(agg_closeness[0][1][v]))
-	
+    print("[=>] Calculando betweenness Temporal")
+    temp_betweenness = compute_temporal_betweenness(G,0,et)
+    for a,b in temp_betweenness.items():
+        dadosOut1.write(str(a)+ " " + str(b)+"\n")
+        
+    print("[=>] Calculando closeness Temporal")
+    temp_closeness = compute_temporal_closeness(G,0,et)
+    for v in G.vertices:
+        dadosOut2.write(str(v) + " " + str(temp_closeness[v]) + "\n")   
+    
+    print("[=>] Calculando closeness Agregado")
+    agg_closeness = compute_static_graph_statistics(G,0,et)
+    for v in G.vertices:
+        print(v + " " + str(agg_closeness[0][1][v]))
+    
     
     dadosIn.close()
     vertex.close()
-    # dadosOut1.close()
-    # dadosOut2.close()
-    # dadosOut3.close()
+    dadosOut1.close()
+    dadosOut2.close()
+    dadosOut3.close()
     print("FIM")
 
 
--- Codigos/Outros/D,MMM,CC.R
+++ Codigos/Outros/D,MMM,CC.R
@@ -3,19 +3,16 @@ library(plyr)
 library(timeordered)
 library(ggplot2)
 library(graphics)
-#library(ProNet)
 
-teste <- read.table("/home/melzinho/Dropbox/Projeto Raquel/Codigos/Graph Metrics/graph.txt", head = T)
-
-#colonia <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/COLONIA/Graph24/communities24T.txt", head = T)
+colonia <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/COLONIA/Graph24/communities24T.txt", head = T)
  
-#fra79 <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/FRANCE/7h-9h/graphVanet-7-9.txt", head = T)
+fra79 <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/FRANCE/7h-9h/graphVanet-7-9.txt", head = T)
  
-#fra1719 <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/FRANCE/17h-19h/graphVanet-17-19.txt", head = T)
-# 
-#A6 <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/MADRID/main/A6/A6.txt", head = T)
-# 
-#M40 <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/MADRID/main/M40/M40.txt", head = T)
+fra1719 <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/FRANCE/17h-19h/graphVanet-17-19.txt", head = T)
+ 
+A6 <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/MADRID/main/A6/A6.txt", head = T)
+ 
+M40 <- read.table("C:/Users/FS-Ma/Dropbox/Projeto Raquel/DataSets + Grafos/MADRID/main/M40/M40.txt", head = T)
 
 diameter_averagePath_cc = function(data,name) {
   
@@ -30,9 +27,9 @@ diameter_averagePath_cc = function(data,name) {
   graphAgg <- as.undirected(graphSlice[1][[1]], mode = c("collapse", "each", "mutual"),edge.attr.comb = igraph_opt("edge.attr.comb"))
   E(graphAgg)$weight <- 1
 
-  #print(paste("Cenario: ", name))
-  #print(paste("Diameter Aggregate =", diameter(graphAgg)))
-  #print(paste("Average Shortest Path Length Agg) =", mean_distance(graphAgg)))
+  print(paste("Cenario: ", name))
+  print(paste("Diameter Aggregate =", diameter(graphAgg)))
+  print(paste("Average Shortest Path Length Agg) =", mean_distance(graphAgg)))
   print(paste("Coeficiente de agrupamento AGG  =", transitivity(graphAgg, type = "global",isolates = c("zero"))))
   
   #inicio temporal CC
@@ -47,14 +44,13 @@ diameter_averagePath_cc = function(data,name) {
     transitivityTemporal = transitivityTemporal + transitivity(staticGraph, type = "global", isolates = c("zero"))
     print(transitivity(staticGraph))
   }
-  print("CC FUNCIONA NO MAIN.TXT")
   print(paste("Coeficiente de agrupamento Temporal  =", transitivityTemporal/timeStop))
   #fim temporal
   print("\n\n############\n\n")  
 }
 diameter_averagePath_cc(teste,"teste")
-# diameter_averagePath_cc(colonia,"Colonia")
-# diameter_averagePath_cc(fra79,"Fra79")
-# diameter_averagePath_cc(fra1719, "Fra1719")
-# diameter_averagePath_cc(A6, "A6")
-# diameter_averagePath_cc(M40,"M40")
+diameter_averagePath_cc(colonia,"Colonia")
+diameter_averagePath_cc(fra79,"Fra79")
+diameter_averagePath_cc(fra1719, "Fra1719")
+diameter_averagePath_cc(A6, "A6")
+diameter_averagePath_cc(M40,"M40")
